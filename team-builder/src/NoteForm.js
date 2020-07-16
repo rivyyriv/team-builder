b176582a4955f887a50c0d78e9379869d7f2410d
River Smith <rivyyriv@gmail.com>
2020-07-16 16:10:12 -0600

    React Comps Complete

--- team-builder/src/NoteForm.js
+++ team-builder/src/NoteForm.js
@@ -2,25 +2,7 @@ import React from "react";
 
 const NoteForm = props => {
 
-  const [title, setTitle] = React.useState("");
-  const [body, setBody] = React.useState("");
-
-  const handleTitle = event => setTitle(event.target.value);
-  const handleBody = event => setBody(event.target.value);
-
-  const handleSubmit = event => {
-    event.preventDefault();
-    if (title && body) {
-      props.setNotes([...props.notes, { title, body }]);
-      resetState();
-    }
-  };
-
-  const resetState = () => {
-    setTitle("");
-    setBody("");
-  };
-
+ 
 
   return (
     <form onSubmit={handleSubmit}>
